51c1e4d0bd9ac8810314ac26c0b1f84566fe6d98
ZackDowning <zackarytdowning@gmail.com>
2021-07-29 20:26:02 -0500

    Added more input validation

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import re
 import time
 
 # TODO: Add comments
+# TODO: Add logging for unexpected errors
+
+# PyInstaller bundle command:
+# pyinstaller -F --hidden-import PySimpleGUI --add-data templates;templates main.py
 
 banner = '   ______                                          ______                             \n'\
          '  / ____/___  ____ ___  ____ ___  ____ _____  ____/ / __ \\__  ______  ____  ___  _____\n'\
@@ -53,51 +57,58 @@ class CommandRunner:
 
         self.mgmt_ips = ManagementFileBrowseWindow().mgmt_ips
         print(banner)
-        self.username = input('Enter Username: ')
-        self.password = getpass('Enter Password: ')
-        self.enable_pw = getpass('(If applicable) Enter Enable Password: ')
-        while True:
-            self.commands = input('(If applicable) Enter config commands seperating each command with a comma and no '
-                                  'space.\n'
-                                  'Example: interface vlan 1,no ip address,shut\n'
-                                  'Commands: ').split(',')
+        if len(self.mgmt_ips) == 0:
+            print('No IP addresses found in file provided.')
+            input('Press Enter to close.')
+        else:
+            self.username = input('Enter Username: ')
+            self.password = getpass('Enter Password: ')
+            self.enable_pw = getpass('(If applicable) Enter Enable Password: ')
             while True:
-                self.save = input('Do you want to save the config? [Y]/N: ')
-                if re.fullmatch(r'[Yy]|', self.save):
-                    self.save = True
+                self.commands = input('(If applicable) Enter config commands seperating each command with a comma and '
+                                      'no space.\n'
+                                      'Example: interface vlan 1,no ip address,shut\n'
+                                      'Commands: ').split(',')
+                while True:
+                    self.save = input('Do you want to save the config? [Y]/N: ')
+                    if re.fullmatch(r'[Yy]|', self.save):
+                        self.save = True
+                        break
+                    elif re.fullmatch(r'[Nn]', self.save):
+                        self.save = False
+                        break
+                print('Testing Connectivity, authentication, and authorization on devices...')
+                start = time.perf_counter()
+                self.check = Connectivity(self.mgmt_ips, self.username, self.password, self.enable_pw)
+                bug_count = 0
+                while True:
+                    self.finished_devices = []
+                    successful_devices = self.check.successful_devices
+                    time.sleep(5)
+                    if bug_count != 0:
+                        print('Ran into bug with Windows multithreading. Trying again...')
+                    print('Sending commands to devices...')
+                    try:
+                        MultiThread(command_runner, successful_devices).mt()
+                        if len(self.finished_devices) == len(successful_devices):
+                            break
+                    except ValueError:
+                        print('Did not recieve ICMP Echo reply from any device.')
+                failed_devices = self.check.failed_devices
+                end = time.perf_counter()
+                print(f'Connectivity check finished in {int(round(end - start, 0))} seconds.')
+                if len(failed_devices) != 0:
+                    print('See failed_devices.csv for more information on failed devices')
+                    output_failed_to_file(failed_devices)
+                print('\nFinished.')
+                more_cmds = input('Do you want to send more commands? Y/[N]: ')
+                if re.fullmatch(r'[Yy]', more_cmds):
+                    continue
+                elif re.fullmatch(r'[Nn]|', more_cmds):
                     break
-                elif re.fullmatch(r'[Nn]', self.save):
-                    self.save = False
+                else:
                     break
-            print('Testing Connectivity, authentication, and authorization on devices...')
-            start = time.perf_counter()
-            self.check = Connectivity(self.mgmt_ips, self.username, self.password, self.enable_pw)
-            bug_count = 0
-            while True:
-                self.finished_devices = []
-                successful_devices = self.check.successful_devices
-                time.sleep(5)
-                if bug_count != 0:
-                    print('Ran into bug with Windows multithreading. Trying again...')
-                print('Sending commands to devices...')
-                MultiThread(command_runner, successful_devices).mt()
-                if len(self.finished_devices) == len(successful_devices):
-                    break
-            failed_devices = self.check.failed_devices
-            end = time.perf_counter()
-            print(f'Connectivity check finished in {int(round(end - start, 0))} seconds.')
-            if len(failed_devices) != 0:
-                print('See failed_devices.csv for more information on failed devices')
-                output_failed_to_file(failed_devices)
-            print('\nFinished.')
-            more_cmds = input('Do you want to send more commands? Y/[N]: ')
-            if re.fullmatch(r'[Yy]', more_cmds):
-                continue
-            elif re.fullmatch(r'[Nn]|', more_cmds):
-                break
-            else:
-                break
-        input('Press Enter to close.')
+            input('Press Enter to close.')
 
 
 if __name__ == '__main__':
